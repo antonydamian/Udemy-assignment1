7ef3a7ea60c6ff01bcc19d62970fe7cec74eaee8
Damian <jantony4@ford.com>
2019-05-18 21:31:55 +0530

    refactored

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -1,14 +1,14 @@
-import { element, by } from "protractor";
-import { Component, OnInit } from "@angular/core";
-import { getParentRenderElement } from "@angular/core/src/view/util";
-import { AngularWaitBarrier } from "blocking-proxy/built/lib/angular_wait_barrier";
-import { getHostElement } from "@angular/core/src/render3";
-import { AngularFontAwesomeModule } from "angular-font-awesome";
+import { element, by } from 'protractor';
+import { Component, OnInit } from '@angular/core';
+import { getParentRenderElement } from '@angular/core/src/view/util';
+import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrier';
+import { getHostElement } from '@angular/core/src/render3';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @Component({
-  selector: "app-rating",
-  templateUrl: "./rating.component.html",
-  styleUrls: ["./rating.component.css"]
+  selector: 'app-rating',
+  templateUrl: './rating.component.html',
+  styleUrls: ['./rating.component.css']
 })
 export class RatingComponent implements OnInit {
   starstate1: boolean = true;
@@ -29,25 +29,25 @@ export class RatingComponent implements OnInit {
     var star_id = event.srcElement.id;
     console.log(star_id);
 
-    if (star_id === "star5") {
+    if (star_id === 'star5') {
       this.starstate5 = !this.starstate5;
       this.starstate4 = !this.starstate4;
       this.starstate3 = !this.starstate3;
       this.starstate2 = !this.starstate2;
       this.starstate1 = !this.starstate1;
-    } else if (star_id === "star4") {
+    } else if (star_id === 'star4') {
       this.starstate4 = !this.starstate4;
       this.starstate3 = !this.starstate3;
       this.starstate2 = !this.starstate2;
       this.starstate1 = !this.starstate1;
-    } else if (star_id === "star3") {
+    } else if (star_id === 'star3') {
       this.starstate3 = !this.starstate3;
       this.starstate2 = !this.starstate2;
       this.starstate1 = !this.starstate1;
-    } else if (star_id === "star2") {
+    } else if (star_id === 'star2') {
       this.starstate2 = !this.starstate2;
       this.starstate1 = !this.starstate1;
-    } else if (star_id === "star1") {
+    } else if (star_id === 'star1') {
       this.starstate1 = !this.starstate1;
     }
   }
@@ -55,9 +55,9 @@ export class RatingComponent implements OnInit {
   toggleClass2(element) {
     for (let i = 1; i <= 5; i++) {
       if (i <= element.id) {
-        document.getElementById(i.toString()).className = "pi pi-star";
+        document.getElementById(i.toString()).className = 'pi pi-star';
       } else {
-        document.getElementById(i.toString()).className = "pi pi-star-o";
+        document.getElementById(i.toString()).className = 'pi pi-star-o';
       }
     }
   }
